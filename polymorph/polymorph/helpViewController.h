923b44a33f6baa9779fdbb8fbcd6774f6d758117
Niklas Nordin <niklas.nordin@nequam.se>
2012-08-18 15:11:32 +0200

    added help placeholder

---
+++ polymorph/polymorph/helpViewController.h
@@ -0,0 +1,13 @@
+//
+//  helpViewController.h
+//  polymorph
+//
+//  Created by Niklas Nordin on 2012-08-18.
+//  Copyright (c) 2012 nequam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface helpViewController : UIViewController
+
+@end
---
+++ polymorph/polymorph/helpViewController.m
@@ -0,0 +1,43 @@
+//
+//  helpViewController.m
+//  polymorph
+//
+//  Created by Niklas Nordin on 2012-08-18.
+//  Copyright (c) 2012 nequam. All rights reserved.
+//
+
+#import "helpViewController.h"
+
+@interface helpViewController ()
+
+@end
+
+@implementation helpViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
