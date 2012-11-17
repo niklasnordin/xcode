80714d9f3679a9f4cd90ea617c9d8db8a5eb17f3
Niklas Nordin <niklas.nordin@nequam.se>
2012-11-17 12:46:04 +0100

    added database class

---
+++ myFlow/myFlow/database.h
@@ -0,0 +1,24 @@
+//
+//  database.h
+//  myFlow
+//
+//  Created by Niklas Nordin on 2012-11-17.
+//  Copyright (c) 2012 Niklas Nordin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface database : NSObject
+
+@property (nonatomic) double dx;
+@property (nonatomic) double dy;
+@property (nonatomic) int nx;
+@property (nonatomic) int ny;
+
+@property (nonatomic) double *u;
+@property (nonatomic) double *v;
+@property (nonatomic) double *p;
+@property (nonatomic) double *rho;
+@property (nonatomic) double *t;
+
+@end
---
+++ myFlow/myFlow/database.m
@@ -0,0 +1,13 @@
+//
+//  database.m
+//  myFlow
+//
+//  Created by Niklas Nordin on 2012-11-17.
+//  Copyright (c) 2012 Niklas Nordin. All rights reserved.
+//
+
+#import "database.h"
+
+@implementation database
+
+@end
