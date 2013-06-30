1e5de5678cb8a0cf86282c610df6336917b9ce8e
Niklas Nordin <niklas.nordin@nequam.se>
2013-06-30 07:37:46 +0200

    added time picker view

---
+++ Checkup Scheduler/Checkup Scheduler/timeSetupViewController.h
@@ -0,0 +1,14 @@
+//
+//  timeSetupViewController.h
+//  Checkup Scheduler
+//
+//  Created by Niklas Nordin on 2013-06-30.
+//  Copyright (c) 2013 Niklas Nordin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface timeSetupViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIPickerView *timePicker;
+
+@end
---
+++ Checkup Scheduler/Checkup Scheduler/timeSetupViewController.m
@@ -0,0 +1,38 @@
+//
+//  timeSetupViewController.m
+//  Checkup Scheduler
+//
+//  Created by Niklas Nordin on 2013-06-30.
+//  Copyright (c) 2013 Niklas Nordin. All rights reserved.
+//
+
+#import "timeSetupViewController.h"
+
+@interface timeSetupViewController ()
+
+@end
+
+@implementation timeSetupViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
