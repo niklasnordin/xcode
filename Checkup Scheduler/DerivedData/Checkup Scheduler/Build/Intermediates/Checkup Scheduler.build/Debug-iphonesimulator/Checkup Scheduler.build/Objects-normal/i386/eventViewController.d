30d36e136ccb404a4cdc6341fd697c99647272f1
Niklas Nordin <niklas.nordin@nequam.se>
2013-09-20 17:48:46 +0200

    no conflicts for calculated button

--- Checkup Scheduler/DerivedData/Checkup Scheduler/Build/Intermediates/Checkup Scheduler.build/Debug-iphonesimulator/Checkup Scheduler.build/Objects-normal/i386/eventViewController.d
+++ Checkup Scheduler/DerivedData/Checkup Scheduler/Build/Intermediates/Checkup Scheduler.build/Debug-iphonesimulator/Checkup Scheduler.build/Objects-normal/i386/eventViewController.d
@@ -4,4 +4,5 @@ dependencies: \
   /Users/niklasnordin/Documents/xcode-github.git/Checkup\ Scheduler/Checkup\ Scheduler/settingsDB.h \
   /Users/niklasnordin/Documents/xcode-github.git/Checkup\ Scheduler/Checkup\ Scheduler/timeSetupViewController.h \
   /Users/niklasnordin/Documents/xcode-github.git/Checkup\ Scheduler/Checkup\ Scheduler/durationPickerView.h \
-  /Users/niklasnordin/Documents/xcode-github.git/Checkup\ Scheduler/Checkup\ Scheduler/reminderPickerView.h
+  /Users/niklasnordin/Documents/xcode-github.git/Checkup\ Scheduler/Checkup\ Scheduler/reminderPickerView.h \
+  /Users/niklasnordin/Documents/xcode-github.git/Checkup\ Scheduler/Checkup\ Scheduler/tasPickerView.h
