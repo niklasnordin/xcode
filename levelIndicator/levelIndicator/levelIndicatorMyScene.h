93df43fd3aee64a72a872f7ac9f6ded18f4bfd7b
Niklas Nordin <niklas.nordin@nequam.se>
2014-01-11 15:09:46 +0100

    added vertical bar

--- levelIndicator/levelIndicator/levelIndicatorMyScene.h
+++ levelIndicator/levelIndicator/levelIndicatorMyScene.h
@@ -9,10 +9,16 @@
 #import <SpriteKit/SpriteKit.h>
 #import <CoreMotion/CoreMotion.h>
 
+static const float rad2deg = 180.0/3.1415926535898;
+
 @interface levelIndicatorMyScene : SKScene
 
+@property (nonatomic) float verticalBarPosition;
+@property (nonatomic) float horisontalBarPosition;
 @property (strong, nonatomic) CMMotionManager *motionManager;
 @property (strong, nonatomic) SKSpriteNode *centerBubble;
+@property (strong, nonatomic) SKSpriteNode *verticalBubble;
+
 @property (strong, nonatomic) SKLabelNode *angleLabel;
 
 @end
--- levelIndicator/levelIndicator/levelIndicatorMyScene.m
+++ levelIndicator/levelIndicator/levelIndicatorMyScene.m
@@ -19,7 +19,8 @@
         self.angleLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
         
         self.angleLabel.text = @"Hello, World!";
-        self.angleLabel.fontSize = 30;
+        self.angleLabel.fontSize = 25;
+        
         self.angleLabel.position = CGPointMake(CGRectGetMidX(self.frame),
                                        CGRectGetMidY(self.frame));
         
@@ -31,6 +32,7 @@
         CGPoint location = CGPointMake(self.frame.origin.x + 0.5*self.frame.size.width, self.frame.origin.y+0.5*self.frame.size.height);
         
         float radius = 25.0;
+        float extraSpace = 3.0;
         self.centerBubble = [SKSpriteNode spriteNodeWithImageNamed:@"hollowBall"];
         self.centerBubble.size = CGSizeMake(2*radius, 2*radius);
         self.centerBubble.position = location;
@@ -38,7 +40,31 @@
         self.centerBubble.physicsBody.dynamic = NO;
         self.centerBubble.physicsBody.affectedByGravity = NO;
         [self addChild:self.centerBubble];
-
+        
+        SKSpriteNode *circle = [SKSpriteNode spriteNodeWithImageNamed:@"circle"];
+        circle.size = CGSizeMake(2*radius+extraSpace*2, 2*radius+extraSpace*2);
+        circle.position = location;
+        [self addChild:circle];
+        
+        self.verticalBarPosition = self.frame.origin.x + 0.1*self.frame.size.width;
+        self.horisontalBarPosition = self.frame.origin.y + 0.1*self.frame.size.height;
+        SKSpriteNode *verticalBar = [SKSpriteNode spriteNodeWithImageNamed:@"square"];
+        verticalBar.size = CGSizeMake(2*radius+extraSpace, 18*radius+extraSpace);
+        CGPoint vBar, hBar;
+        vBar.x = self.verticalBarPosition;
+        vBar.y = location.y;
+        hBar.x = location.x;
+        hBar.y = self.horisontalBarPosition;
+        verticalBar.position = vBar;
+        
+        [self addChild:verticalBar];
+        
+        self.verticalBubble = [SKSpriteNode spriteNodeWithImageNamed:@"hollowBall"];
+        self.verticalBubble.size = CGSizeMake(2*radius, 2*radius);
+        self.verticalBubble.position = vBar;
+        self.verticalBubble.alpha = 0.5;
+        
+        [self addChild:self.verticalBubble];
     }
     return self;
 }
@@ -63,7 +89,22 @@
     /* Called before each frame is rendered */
     
     CMAcceleration gravity = self.motionManager.deviceMotion.gravity;
-
+    CMAttitude *att = self.motionManager.deviceMotion.attitude;
+    
+    // gMag should be 1.0
+    float gMag = sqrtf(gravity.x*gravity.x + gravity.y*gravity.y + gravity.z*gravity.z);
+    float at = gravity.x/gMag;
+    float bt = gravity.y/gMag;
+    
+    // sidvinkel
+    double roll = att.roll*rad2deg;
+    // fram/bak vinkel
+    double pitch = att.pitch*rad2deg;
+    // yaw är rotationsvinkeln runt gravitations-riktingen
+    double yaw = att.yaw*rad2deg;
+    
+    self.angleLabel.text = [NSString stringWithFormat:@"%4.1f deg",roll];
+    
     CGPoint center;
     center.x = self.frame.origin.x + 0.5*self.frame.size.width;
     center.y = self.frame.origin.y + 0.5*self.frame.size.height;
@@ -71,7 +112,15 @@
     CGPoint pos;
     pos.x = center.x - 0.5*self.frame.size.width*gravity.x;
     pos.y = center.y - 0.5*self.frame.size.height*gravity.y;
+    
     self.centerBubble.position = pos;
+    
+    CGPoint vPos;
+    vPos.x = self.verticalBubble.position.x;
+    vPos.y = pos.y;
+    
+    self.verticalBubble.position = vPos;
+    
 }
 
 @end
