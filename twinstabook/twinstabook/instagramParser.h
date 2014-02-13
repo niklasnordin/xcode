9bb4781b4688f429175690081bd3840a368bc7cc
Niklas Nordin <niklas.nordin@nequam.se>
2014-02-13 17:51:02 +0100

    added empty parser templates

---
+++ twinstabook/twinstabook/instagramParser.h
@@ -0,0 +1,13 @@
+//
+//  instagramParser.h
+//  twinstabook
+//
+//  Created by Niklas Nordin on 2014-02-13.
+//  Copyright (c) 2014 Niklas Nordin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface instagramParser : NSObject
+
+@end
---
+++ twinstabook/twinstabook/twitterParser.m
@@ -0,0 +1,13 @@
+//
+//  twitterParser.m
+//  twinstabook
+//
+//  Created by Niklas Nordin on 2014-02-13.
+//  Copyright (c) 2014 Niklas Nordin. All rights reserved.
+//
+
+#import "twitterParser.h"
+
+@implementation twitterParser
+
+@end
