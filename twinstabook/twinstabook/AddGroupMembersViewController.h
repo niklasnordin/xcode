a4c17ee1aede21c037cc24bce7528db1911ff1ce
Niklas Nordin <niklas.nordin@nequam.se>
2014-02-23 13:27:54 +0100

    working on new layout for adding new group members

---
+++ twinstabook/twinstabook/AddGroupMembersViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddGroupMembersViewController.h
+//  twinstabook
+//
+//  Created by Niklas Nordin on 2014-02-23.
+//  Copyright (c) 2014 Niklas Nordin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddGroupMembersViewController : UIViewController
+
+@end
---
+++ twinstabook/twinstabook/AddGroupMembersViewController.m
@@ -0,0 +1,38 @@
+//
+//  AddGroupMembersViewController.m
+//  twinstabook
+//
+//  Created by Niklas Nordin on 2014-02-23.
+//  Copyright (c) 2014 Niklas Nordin. All rights reserved.
+//
+
+#import "AddGroupMembersViewController.h"
+
+@interface AddGroupMembersViewController ()
+
+@end
+
+@implementation AddGroupMembersViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
