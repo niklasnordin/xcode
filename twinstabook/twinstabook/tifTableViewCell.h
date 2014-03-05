2e96cb57e5c976f27ed225f880c9b0bb66968c62
Niklas Nordin <niklas.nordin@nequam.se>
2014-03-05 08:21:51 +0100

    connected the objects to the table view class

--- twinstabook/twinstabook/tifTableViewCell.h
+++ twinstabook/twinstabook/tifTableViewCell.h
@@ -9,5 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface tifTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *messageLabel;
+@property (weak, nonatomic) IBOutlet UILabel *likesLabel;
+@property (weak, nonatomic) IBOutlet UILabel *commentsLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *mainImage;
+@property (weak, nonatomic) IBOutlet UIImageView *typeImage;
+@property (weak, nonatomic) IBOutlet UIImageView *profileImage;
+@property (weak, nonatomic) IBOutlet UIImageView *likesImage;
+@property (weak, nonatomic) IBOutlet UIImageView *commentsImage;
 
 @end
